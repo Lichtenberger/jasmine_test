ac1834619d6cccbcb37b52d60f0b3204e4a85adc
Mike <124381569+Lichtenberger@users.noreply.github.com>
2023-08-06 08:40:26 -0600

    update helper.test
    first version of the helper test

--- helpers.test.js
+++ helpers.test.js
@@ -1,58 +1,26 @@
-//sumPaymentTotal test
-describe("sumPaymentTotal(type)", function() {
-    it("should return the sum of all tipAmt values", function() {
-      // Set up initial state
-      allPayments = {
-        payment1: {tipAmt: '5'},
-        payment2: {tipAmt: '10'},
-        payment3: {tipAmt: '15'}
-      };
-  
-      // Call function to test
-      let result = sumPaymentTotal('tipAmt');
-  
-      // Check if result is correct
-      expect(result).toBe(30);
-    });
-  
-    it("should return the sum of all billAmt values", function() {
-      // Set up initial state
-      allPayments = {
-        payment1: {billAmt: '50'},
-        payment2: {billAmt: '100'},
-        payment3: {billAmt: '150'}
-      };
-  
-      // Call function to test
-      let result = sumPaymentTotal('billAmt');
-  
-      // Check if result is correct
-      expect(result).toBe(300);
-    });
+describe('Testing the payment total', function() {
+  beforeEach(function() {
+    total.value = 10;
   });
 
-  //calculateTipPercent test checks the tip percentage
-  describe("calculateTipPercent(billAmt, tipAmt)", function() {
-    it("should return the correct tip percentage", function() {
-      // Call function to test
-      let result = calculateTipPercent(100, 20);
-  
-      // Check if result is correct
-      expect(result).toBe(20);
-    });
+  it("accepts 'tipAmt' and sums total", function() {
+    expect(Object.keys(total)).toEqual(10);
   });
-  
-  //make sure table creates properly
-  describe("appendTd(tr, value)", function() {
-    it("should append a new td element to the tr element", function() {
-      // Set up initial state
-      let tr = document.createElement('tr');
-  
-      // Call function to test
-      appendTd(tr, 'Test');
-  
-      // Check if tr has a new td element
-      expect(tr.children.length).toBe(1);
-    });
+
+  afterEach(function() {
+    total.value = 0;
   });
-  
+});
+
+describe('converts the bill and tip amount into a tip percent', function() {
+  beforeEach(function() {
+    total.value = (100 / (50 / 5));
+  });
+  it('converts the bill and tip amount into a tip percent calculateTipPercent()', function() {
+    expect(total.value).toBeDefined();
+    expect(total.value).toEqual(10);
+  });
+  afterEach(function() {
+    total.value = 0;
+  })
+})
